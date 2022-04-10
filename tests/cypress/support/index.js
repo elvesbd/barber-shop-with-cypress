f6feb01bbdde9c11e56a6a1528763ea1b890ea7b
elvesbd <elvesbd@yahoo.com>
2022-04-10 12:04:38 -0300

    add refrence types Cypress

--- tests/cypress/support/index.js
+++ tests/cypress/support/index.js
@@ -1,3 +1,5 @@
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
 // ***********************************************************
 // This example support/index.js is processed and
 // loaded automatically before your test files.
