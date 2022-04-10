02779453a925ff4cde3ecdc881dd12dd36afe956
elvesbd <elvesbd@yahoo.com>
2022-04-10 15:12:59 -0300

    create objecr user

--- tests/cypress/integration/signup.spec.js
+++ tests/cypress/integration/signup.spec.js
@@ -1,19 +1,21 @@
 describe('signup', () => {
-  const name = 'Ayla Brito'
-  const email = 'ayla@gmail.com'
-  const password = '123456'
+  const user = {
+    name: 'Ayla Brito',
+    email: 'ayla@gmail.com',
+    password: '123456',
+  }
 
   it('should register a new user', () => {
-    cy.task('removeUser', email)
+    cy.task('removeUser', user.email)
       .then((result) => {
         console.log(result)
       })
   
     cy.visit('/signup')
     
-    cy.get('input[placeholder="Nome"]').type(name)
-    cy.get('input[placeholder="E-mail"]').type(email)
-    cy.get('input[placeholder="Senha"]').type(password)
+    cy.get('input[placeholder="Nome"]').type(user.name)
+    cy.get('input[placeholder="E-mail"]').type(user.email)
+    cy.get('input[placeholder="Senha"]').type(user.password)
   
     cy.contains('button', 'Cadastrar').click()
     
@@ -26,9 +28,9 @@ describe('signup', () => {
   it('should not register a new user and display toast with error message', () => {
     cy.visit('/signup')
     
-    cy.get('input[placeholder="Nome"]').type(name)
-    cy.get('input[placeholder="E-mail"]').type(email)
-    cy.get('input[placeholder="Senha"]').type(password)
+    cy.get('input[placeholder="Nome"]').type(user.name)
+    cy.get('input[placeholder="E-mail"]').type(user.email)
+    cy.get('input[placeholder="Senha"]').type(user.password)
   
     cy.contains('button', 'Cadastrar').click()
     
