f8d2e9a380c159e443fef4b342669ec5e465b9e1
elvesbd <elvesbd@yahoo.com>
2022-04-10 12:55:03 -0300

    creating new enrollment strategy

--- tests/cypress/integration/signup.spec.js
+++ tests/cypress/integration/signup.spec.js
@@ -14,14 +14,7 @@ it('should register a new user', () => {
   cy.get('input[placeholder="E-mail"]').type(email)
   cy.get('input[placeholder="Senha"]').type(password)
 
-  /* interceptando a chamada api para evitar que o cadastro seja efetuado no banco de dados
-  cy.intercept('POST', '/users', {
-    statusCode: 200,
-  }).as('postUser') */
-
   cy.contains('button', 'Cadastrar').click()
-
-  //cy.wait('@postUser')
   
   cy.get('.toast')
     .should('be.visible')
