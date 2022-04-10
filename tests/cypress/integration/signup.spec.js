0a97c8a318617ba722357141cbaea83a0c1fe754
elvesbd <elvesbd@yahoo.com>
2022-04-10 15:49:55 -0300

    implementing contexts

--- tests/cypress/integration/signup.spec.js
+++ tests/cypress/integration/signup.spec.js
@@ -1,58 +1,65 @@
 describe('signup', () => {
-  it('should register a new user', () => {
+  context('when the user is new', () => {
     const user = {
       name: 'Ayla Brito',
       email: 'ayla@gmail.com',
       password: '123456',
     }
-    // remove a massa de teste criado no banco no teste anterior antes de rodar o novo teste
-    cy.task('removeUser', user.email)
+
+    before(() => {
+      cy.task('removeUser', user.email)
       .then((result) => {
         console.log(result)
       })
+    })
+
+    it('should register a new user with success', () => {
+      cy.visit('/signup')
   
-    cy.visit('/signup')
-    
-    cy.get('input[placeholder="Nome"]').type(user.name)
-    cy.get('input[placeholder="E-mail"]').type(user.email)
-    cy.get('input[placeholder="Senha"]').type(user.password)
-  
-    cy.contains('button', 'Cadastrar').click()
-    
-    cy.get('.toast')
-      .should('be.visible')
-      .find('p')
-      .should('have.text', 'Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!')
-  });
-  
-  it('should not register a new user and display toast with error message', () => {
+      cy.get('input[placeholder="Nome"]').type(user.name)
+      cy.get('input[placeholder="E-mail"]').type(user.email)
+      cy.get('input[placeholder="Senha"]').type(user.password)
+      cy.contains('button', 'Cadastrar').click()
+      
+      cy.get('.toast')
+        .should('be.visible')
+        .find('p')
+        .should('have.text', 'Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!')
+    });
+  })
+
+  context('when the email already exists in the base data', () => {
     const user = {
       name: 'John',
       email: 'john@example.com',
       password: '123456',
       is_provider: true
     }
-    // remove a massa de teste criado no banco no teste anterior antes de rodar o novo teste
-    cy.task('removeUser', user.email)
-      .then((result) => {
-        console.log(result)
+
+    before(() => {
+      cy.task('removeUser', user.email)
+        .then((result) => {
+          console.log(result)
       })
 
-    cy.request('POST', 'http://localhost:3333/users', user).then((response) => {
-      expect(response.status).to.eq(200)
+      cy.request('POST', 'http://localhost:3333/users', user).then((response) => {
+        expect(response.status).to.eq(200)
+      })
     })
 
-    cy.visit('/signup')
+    it('should not register a new user and display toast with error message', () => {
+      cy.visit('/signup')
+      
+      cy.get('input[placeholder="Nome"]').type(user.name)
+      cy.get('input[placeholder="E-mail"]').type(user.email)
+      cy.get('input[placeholder="Senha"]').type(user.password)
     
-    cy.get('input[placeholder="Nome"]').type(user.name)
-    cy.get('input[placeholder="E-mail"]').type(user.email)
-    cy.get('input[placeholder="Senha"]').type(user.password)
-  
-    cy.contains('button', 'Cadastrar').click()
-    
-    cy.get('.toast')
-      .should('be.visible')
-      .find('p')
-      .should('have.text', 'Email já cadastrado para outro usuário.')
-  });
+      cy.contains('button', 'Cadastrar').click()
+      
+      cy.get('.toast')
+        .should('be.visible')
+        .find('p')
+        .should('have.text', 'Email já cadastrado para outro usuário.')
+    });
+  })
 })
