9c219c0fd9f76a1278f1535134f24485d2581ee1
elvesbd <elvesbd@yahoo.com>
2022-04-10 12:04:59 -0300

    create test signup

--- tests/cypress/integration/signup.spec.js
+++ tests/cypress/integration/signup.spec.js
@@ -1,3 +1,20 @@
-it('', () => {
+import faker from '@faker-js/faker'
+
+it('should register a new user', () => {
+  const name = 'Ayla Brito'
+  const email = faker.internet.email()
+  const password = '123456'
+
+  cy.visit('/signup')
   
+  cy.get('input[placeholder="Nome"]').type(name)
+  cy.get('input[placeholder="E-mail"]').type(email)
+  cy.get('input[placeholder="Senha"]').type(password)
+
+  cy.contains('button', 'Cadastrar').click()
+
+  cy.get('.toast')
+    .should('be.visible')
+    .find('p')
+    .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
 });
