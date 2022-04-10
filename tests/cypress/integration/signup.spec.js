aad0a98482dc1e1214314ec7311281a61cee8d14
elvesbd <elvesbd@yahoo.com>
2022-04-10 13:17:06 -0300

    grouping scenarios with describe

--- tests/cypress/integration/signup.spec.js
+++ tests/cypress/integration/signup.spec.js
@@ -1,42 +1,44 @@
-it('should register a new user', () => {
-  const name = 'Ayla Brito'
-  const email = 'ayla@gmail.com'
-  const password = '123456'
-
-  cy.task('removeUser', email)
-    .then((result) => {
-      console.log(result)
-    })
-
-  cy.visit('/signup')
+describe('signup', () => {
+  it('should register a new user', () => {
+    const name = 'Ayla Brito'
+    const email = 'ayla@gmail.com'
+    const password = '123456'
   
-  cy.get('input[placeholder="Nome"]').type(name)
-  cy.get('input[placeholder="E-mail"]').type(email)
-  cy.get('input[placeholder="Senha"]').type(password)
-
-  cy.contains('button', 'Cadastrar').click()
+    cy.task('removeUser', email)
+      .then((result) => {
+        console.log(result)
+      })
   
-  cy.get('.toast')
-    .should('be.visible')
-    .find('p')
-    .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
-});
-
-it('should not register a new user and display toast with error message', () => {
-  const name = 'Ayla Brito'
-  const email = 'ayla@gmail.com'
-  const password = '123456'
-
-  cy.visit('/signup')
+    cy.visit('/signup')
+    
+    cy.get('input[placeholder="Nome"]').type(name)
+    cy.get('input[placeholder="E-mail"]').type(email)
+    cy.get('input[placeholder="Senha"]').type(password)
   
-  cy.get('input[placeholder="Nome"]').type(name)
-  cy.get('input[placeholder="E-mail"]').type(email)
-  cy.get('input[placeholder="Senha"]').type(password)
-
-  cy.contains('button', 'Cadastrar').click()
+    cy.contains('button', 'Cadastrar').click()
+    
+    cy.get('.toast')
+      .should('be.visible')
+      .find('p')
+      .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
+  });
   
-  cy.get('.toast')
-    .should('be.visible')
-    .find('p')
-    .should('have.text', 'Email já cadastrado para outro usuário.')
-});
+  it('should not register a new user and display toast with error message', () => {
+    const name = 'Ayla Brito'
+    const email = 'ayla@gmail.com'
+    const password = '123456'
+  
+    cy.visit('/signup')
+    
+    cy.get('input[placeholder="Nome"]').type(name)
+    cy.get('input[placeholder="E-mail"]').type(email)
+    cy.get('input[placeholder="Senha"]').type(password)
+  
+    cy.contains('button', 'Cadastrar').click()
+    
+    cy.get('.toast')
+      .should('be.visible')
+      .find('p')
+      .should('have.text', 'Email já cadastrado para outro usuário.')
+  });
+})
