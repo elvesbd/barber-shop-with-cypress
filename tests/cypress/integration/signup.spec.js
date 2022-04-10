83a34fcfcbec6a33b89bbd04ee723df10fcad990
elvesbd <elvesbd@yahoo.com>
2022-04-10 13:13:46 -0300

    create teste an error toast if the user is already registered

--- tests/cypress/integration/signup.spec.js
+++ tests/cypress/integration/signup.spec.js
@@ -21,3 +21,22 @@ it('should register a new user', () => {
     .find('p')
     .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
 });
+
+it('should not register a new user and display toast with error message', () => {
+  const name = 'Ayla Brito'
+  const email = 'ayla@gmail.com'
+  const password = '123456'
+
+  cy.visit('/signup')
+  
+  cy.get('input[placeholder="Nome"]').type(name)
+  cy.get('input[placeholder="E-mail"]').type(email)
+  cy.get('input[placeholder="Senha"]').type(password)
+
+  cy.contains('button', 'Cadastrar').click()
+  
+  cy.get('.toast')
+    .should('be.visible')
+    .find('p')
+    .should('have.text', 'Email já cadastrado para outro usuário.')
+});
