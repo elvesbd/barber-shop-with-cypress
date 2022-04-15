a53bf7dec85d4087586604f648dc34f49de2ab9f
elvesbd <elvesbd@yahoo.com>
2022-04-15 18:10:42 -0300

    fix: add config db in cypress.json

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -22,13 +22,8 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
-  const pool = new Pool({
-    host: 'salt.db.elephantsql.com',
-    user: 'jerznhkt',
-    password: '6UnQ5tefd731Y1WGaz-Xx-JnskCzZpZJ',
-    database: 'jerznhkt',
-    port: 5432
-  })
+  const configJson = require(config.configFile)
+  const pool = new Pool(configJson.dbConfig)
 
   on('task', {
     removeUser(email) {
