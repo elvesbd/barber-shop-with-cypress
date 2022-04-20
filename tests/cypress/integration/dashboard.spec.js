7aefb8892441cc31728c89a2f5d00c49105b8ab3
elvesbd <elvesbd@yahoo.com>
2022-04-19 21:44:21 -0300

    fix: created commands

--- tests/cypress/integration/dashboard.spec.js
+++ tests/cypress/integration/dashboard.spec.js
@@ -1,3 +1,5 @@
+import moment from 'moment'
+
 describe('dashboard', function() {
   context('when the user makes an appointment in the mobile app', () => {
     const data = {
@@ -7,7 +9,7 @@ describe('dashboard', function() {
         password: 'pwd123',
         is_provider: false,
       },
-      samurai: {
+      provider: {
         name: 'Ramon Valdes',
         email: 'ramos@televisa.com',
         password: 'pwd123',
@@ -16,17 +18,49 @@ describe('dashboard', function() {
     }
 
     before(function() {
+      cy.postUser(data.provider)
       cy.postUser(data.customer)
-      cy.postUser(data.samurai)
 
       cy.apiLogin(data.customer)
       cy.log(`Pegamos o token ${Cypress.env('apiToken')}`)
+
+      cy.setProviderId(data.provider.email)
     })
 
-    it('appointment should be displayed on the dashboard', () => {
-      console.log(data)
+    it('appointment should be displayed on the dashboard', function() {
+      cy.log(`Id do ramon é ${Cypress.env('providerId')}`)
+      cy.createAppointment()
     });
   });
+})
+
+Cypress.Commands.add('createAppointment', function() {
+  let now = new Date()
+
+  now.setDate(now.getDate() + 1)
+
+  const day = moment(now).format('YYYY-MM-DD 14:00:00')
+  cy.log(day)
+})
+
+Cypress.Commands.add('setProviderId', function(providerEmail) {
+  cy.request({
+    method: 'GET',
+    url: 'http://localhost:3333/providers',
+    headers: {
+      'authorization': `Bearer ${Cypress.env('apiToken')}` 
+    }
+  }).then(function(response) {
+    expect(response.status).to.eq(200)
+    console.log(response.body)
+    const providerList = response.body
+
+    providerList.forEach(function(provider) {
+      if (provider.email === providerEmail) {
+        Cypress.env('providerId', provider.id)
+      }
+    })
+  })
 })
 
 Cypress.Commands.add('apiLogin', function(user) {
